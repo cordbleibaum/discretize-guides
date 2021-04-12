f371f3438016558eccad06670ad430edd9dc531f
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2021-04-12 13:09:45 +0200

    use vcpkg package manager

--- client/application/main.cpp
+++ client/application/main.cpp
@@ -1,7 +1,26 @@
 #include <cstdlib>
+#include <map>
+#include <functional>
+#include <string>
+#include <iostream>
+
+#include <nlohmann/json.hpp>
 
 #include "../benchmarks/ram.h"
 
+using json = nlohmann::json;
+
 int main() {
+    std::map<std::string, std::function<double()>> benchmarks;
+    json results;
+
+    benchmarks["ram_bandwidth_multicore"] = benchfactor::benchmarks::ram_bandwidth_multicore;
+
+    for (const auto& [name, function] : benchmarks) {
+        results[name] = function();
+    }
+
+    std::cout << results.dump(4);
+
     return EXIT_SUCCESS;
 }
