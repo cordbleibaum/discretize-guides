42a3845ebd4d683e23811ef94e72a65374124447
Cord Bleibaum <cord.bleibaum@benchfactor.com>
2021-04-12 21:38:22 +0200

    wip c++ client

--- api/index.js
+++ api/index.js
@@ -1,29 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
-const MongoClient = require('mongodb').MongoClient;
+const { ObjectId, MongoClient } = require('mongodb');
 const amqp = require('amqplib');
 const assert = require('assert');
 
 const typeDefs = gql`
     type System {
-        id: ID!
-        parts: [Part]!
-    }
-
-    type Part {
-        id: ID!
-        name: String
-        number: String
-        image: String
-    }
-
-    type Benchmark {
-        id: ID!
-        system: System!
-        ram_bandwidth: int
+        id: ID
     }
 
     type Query {
-        benchmark(id: ID!): [Benchmark]
+        system(id: ID): System
+    }
+
+    type Mutation {
+        createSystemID: System
     }
 `;
 
@@ -38,8 +28,22 @@ const typeDefs = gql`
 
     const resolvers = {
         Query: {
-            benchmark: (id) => { },
+            system: async (_, { id }, context) => {
+                const system = await db.collection('systems').findOne({
+                    _id: ObjectId(id)
+                })
+                return {
+                    id: system._id
+                }
+            }
         },
+        Mutation: {
+            createSystemID: async () => {
+                let emptySystem = {}
+                const result = await db.collection('systems').insertOne(emptySystem);
+                return { "id": result.insertedId};
+            }
+        }
     };
 
     const server = new ApolloServer({ typeDefs, resolvers });
--- client/application/main.cpp
+++ client/application/main.cpp
@@ -3,12 +3,73 @@
 #include <functional>
 #include <string>
 #include <iostream>
+#include <filesystem>
 
+#include <curl/curl.h>
 #include <nlohmann/json.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #include "../benchmarks/ram.h"
 
 using json = nlohmann::json;
+namespace pt = boost::property_tree;
+
+const std::string settings_filename = "settings.json";
+
+std::string graphql_call(std::string server, std::string data) {
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        std::string returnBuffer;
+
+        struct curl_slist *headers=NULL;
+        headers = curl_slist_append(headers, "charsets: utf-8");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        headers = curl_slist_append(headers, "Accept: application/json");
+        curl_easy_setopt(curl, CURLOPT_URL, server);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"query\" : \"" + data + "\"}");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [](char *contents, size_t size, size_t nmemb, void *userp) {
+            ((std::string*)userp)->append((char*)contents, size * nmemb);
+            return size * nmemb;
+        });
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &returnBuffer);
+        curl_easy_perform(curl);
+
+        std::cout << returnBuffer << "test" << std::endl;
+
+        curl_easy_cleanup(curl);
+    }
+    return "";
+}
+
+std::string get_system_id() {
+    std::string system_id = "";
+
+    if (std::filesystem::exists(settings_filename)) {
+        pt::ptree tree;
+        pt::read_json(settings_filename, tree);
+        system_id = tree.get<std::string>(settings_filename, "");
+    }
+
+    if (system_id == "") {
+        graphql_call("localhost:4000", "\
+            mutation addSystem {\
+                createSystemID{\
+                    id\
+                }\
+            }\
+        ");
+    }
+
+    return system_id;
+}
+
+void save_system_id(std::string system_id) {
+    pt::ptree tree;
+    tree.add("system_id", system_id);
+    pt::write_json(settings_filename, tree);
+}
 
 int main() {
     std::map<std::string, std::function<double()>> benchmarks;
@@ -22,5 +83,10 @@ int main() {
 
     std::cout << results.dump(4);
 
+    std::string system_id = get_system_id();
+
+    // TODO
+
+    save_system_id(system_id);
     return EXIT_SUCCESS;
 }
